f303c13504d2967ec74f06603349dc077e689bac
Graham Nickbel <134581805+grahamnickbel23@users.noreply.github.com>
2023-11-17 22:41:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ document.addEventListener("DOMContentLoaded", function () {
         // Hide the splash screen
         document.getElementById("splash-screen").style.display = "none";
     }, 7000); // Adjust the duration as needed
+
+    //code for changing color of the navbar
+    var navbar = document.querySelector('.main-header');
+
+    window.addEventListener('scroll', function () {
+        // Check the scroll position
+        if (window.scrollY > 1000) { // Adjust the scroll position as needed
+            navbar.style.backgroundColor = '#000'; // Change to the desired color
+        } else {
+            navbar.style.backgroundColor = 'transparent'; // Reset to the initial color
+        }
+    });
 });
