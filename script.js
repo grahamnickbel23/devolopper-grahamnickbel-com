b8c0efe40743b794c1bcca303ff56aad129eed7d
Graham Nickbel <134581805+grahamnickbel23@users.noreply.github.com>
2023-11-17 19:19:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Simulate a delay (e.g., 2 seconds) to show the splash screen
+    setTimeout(function () {
+        // Hide the splash screen
+        document.getElementById("splash-screen").style.display = "none";
+    }, 7000); // Adjust the duration as needed
+});
